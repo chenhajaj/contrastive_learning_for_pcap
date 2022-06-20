562a127ac26fa6438c1d4b2bdba4198bad6dc9a1
Rotem Bar <rotembaruch@gmail.com>
2022-06-20 17:55:32 +0300

    54

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='src',
-    packages = ['src'],
+    name='contrastive_learning_for_pcap',
+    packages = ['contrastive_learning_for_pcap'],
     version='0.3',
     license='MIT',
     author="rotem_bar",
     author_email='rotembaruch@gmail.com',
     url='https://github.com/rotembaruch/contrastive_learning_for_pcap',
-    download_url = 'https://github.com/rotembaruch/contrastive_learning_for_pcap/archive/refs/tags/V0.3.tar.gz',
+    download_url = 'https://github.com/rotembaruch/contrastive_learning_for_pcap/archive/refs/tags/V0.4.tar.gz',
     keywords='clfp',
     install_requires=[
           'transformers',
